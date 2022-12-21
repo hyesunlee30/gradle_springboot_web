c08679634e230663d1a52c1a3ec17f322694593e
hyesunlee30 <sunnylee3771@gmail.com>
2022-12-21 17:17:41 +0900

    createform

---
+++ src/main/java/gradle/spring/SpringConfig.java
@@ -0,0 +1,21 @@
+package gradle.spring;
+
+import gradle.spring.repository.MemberRepository;
+import gradle.spring.repository.MemoryMemberRepository;
+import gradle.spring.service.MemberService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringConfig {
+
+    @Bean
+    public MemberService memberService() {
+        return new MemberService(memberRepository());
+    }
+
+    @Bean
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+}
--- src/main/java/gradle/spring/service/MemberService.java
+++ src/main/java/gradle/spring/service/MemberService.java
@@ -2,6 +2,8 @@ package gradle.spring.service;
 
 import gradle.spring.domain.Member;
 import gradle.spring.repository.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
